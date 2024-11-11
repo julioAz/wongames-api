ae85a48141fddb02e206ad22fabbed290a596f2f
Julio Azevedo <azevedo.developer@gmail.com>
2024-11-11 08:57:33 -0300

    adding methods and functions to api/games/populate

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -4,6 +4,52 @@
 
 import axios from 'axios';
 import { factories } from '@strapi/strapi';
+import { JSDOM } from 'jsdom'
+import slugify from 'slugify'
+import { resourceLimits } from 'worker_threads';
+
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll('-', '_').toLowerCase();
+
+  const body = await axios.get(`https://www.gog.com/game/${gogSlug}`);
+  const dom = new JSDOM(body.data)
+  const raw_description = dom.window.document.querySelector('.description')
+  const description = raw_description.innerHTML
+  const shortDescription = raw_description.textContent.slice(0, 160)
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
+  return {
+    description,
+    shortDescription,
+    rating: ratingElement
+      ? ratingElement
+        .getAttribute("xlink:href")
+        .replace(/_/g, "")
+        .replace("#", "") : "BR0"
+  }
+}
+
+async function getByName(name, entityService) {
+  const item = await strapi.service(entityService).find({
+    filters: { name }
+  })
+
+  return item.results.length > 0 ? item.results[0] : null;
+}
+
+async function create(name, entityService) {
+  const item = await getByName(name, entityService)
+  if (item) {
+    await strapi.service(entityService).create({
+      data: {
+        name,
+        slug: slugify(name, { strict: true, lower: true }),
+      }
+    })
+  }
+}
 
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
@@ -12,6 +58,16 @@ export default factories.createCoreService('api::game.game', () => ({
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products[1]);
+    products[0].developers.map(async (developer) => {
+      await create(developer, "api::developer.developer")
+    });
+
+    products[0].publishers.map(async (publisher) => {
+      await create(publisher, "api::publisher.publisher")
+    });
+
+    products[0].genres.map(async ({name}) => {
+      await create(name, "api::category.category")
+    });
   }
 }));
